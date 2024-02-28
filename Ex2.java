8136499fe58893d3370b307168d2f0698f32467b
sureshsathri <sureshsathri4@gmail.com>
2024-02-28 10:22:17 +0530

    ex2 file

---
+++ Ex2.java
@@ -0,0 +1,18 @@
+//program to check if a given number is even or odd
+import java.util.*;
+public class Ex2 {
+	public static void main(String[] args) {
+		int Number=27;
+		// TODO Auto-generated method stub 
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		if(Number%2==0)
+			System.out.println(Number +"is even");
+		else 
+			System.out.println(Number +"is odd");
+		
+		
+
+	}
+
+}
