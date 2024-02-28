a1107996410ba3a4ad548b1807d4356827d4a996
sureshsathri <sureshsathri4@gmail.com>
2024-02-28 10:22:43 +0530

    ex3 file

---
+++ Ex3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ex3 {
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		String s1 ="Mumbai";
+		String s2 ="Bangalore";
+		System.out.println(s1+ ","+s2);
+	}
+}
