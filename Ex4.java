dd5d6eb16b9a35c31e2a41752ab50d79de6ba577
sureshsathri <sureshsathri4@gmail.com>
2024-02-28 10:22:59 +0530

    ex4 file

---
+++ Ex4.java
@@ -0,0 +1,14 @@
+//program initialize two character variable 
+
+public class Ex4 {
+	public static void main(String[] args)
+	{
+		char item1='s';
+		char item2='e';
+		if(item1>item2)
+			System.out.print(item2+","+item1);
+		else
+			System.out.print(item1+","+item2);
+	}
+	}
+		
